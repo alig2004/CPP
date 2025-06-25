f885d055c8b2a0ccd8f1de2183adfa88b457a6de
Alison Gleeson <agleeson987@gmail.com>
2025-06-25 19:47:52 +0100

    Problem 3

---
+++ Variables and Data Types/3-Problem3.cpp
@@ -0,0 +1,16 @@
+/*
+Initialise a global variable, g, of type int and value 10
+*/
+
+#include <iostream>
+using namespace std;
+
+// declare a global variable g
+int g = 10;
+
+int main() {
+    // printing the value of global variable g
+    cout << g << endl;
+
+    return 0;
+}
