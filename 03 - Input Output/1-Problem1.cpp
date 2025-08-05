9e5d36fb8f56a5e3f7bd0dfe99e778ecff026241
Alison Gleeson <agleeson987@gmail.com>
2025-08-05 19:35:06 +0100

    Problem 1

---
+++ 03 - Input Output/1-Problem1.cpp
@@ -0,0 +1,14 @@
+/*
+Write "Geeks for Geeks" with each word on a separate line
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // print Geeks for Geeks in seperate line
+    cout << "Geeks\nfor\nGeeks";
+
+    return 0;
+}
