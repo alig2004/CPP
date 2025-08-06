7533319a147ea4f1025022d370214ee8d4d3695f
Alison Gleeson <agleeson987@gmail.com>
2025-08-06 18:40:29 +0100

    Problem 8

---
+++ 03 - Input Output/8-Problem8.cpp
@@ -0,0 +1,15 @@
+/*
+Given an integer, a, print it in hexadecimal format followed by a new line
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 20;
+    cout << std::hex;
+
+    cout << a << endl;
+
+    return 0;
+}
