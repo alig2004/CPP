6d05b9c4cee5d6bc8e94bf28679fe0eb46d707d6
Alison Gleeson <agleeson987@gmail.com>
2025-08-06 18:24:10 +0100

    Problem 7

---
+++ 03 - Input Output/7-Problem7.cpp
@@ -0,0 +1,12 @@
+/*
+Print "Hello World", ensuring the double quotes are displayed
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "\"Hello World\"";
+
+    return 0;
+}
