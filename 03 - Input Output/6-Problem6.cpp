2c31ae7d0c87b6cf07d615a2692f91f002351598
Alison Gleeson <agleeson987@gmail.com>
2025-08-06 18:19:30 +0100

    Problem 6

---
+++ 03 - Input Output/6-Problem6.cpp
@@ -0,0 +1,14 @@
+/*
+Given an inputted number, n, add 10 to n and display it
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "Result is: " << (n+10);
+}
