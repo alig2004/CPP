44adb39893b1e83edefed35284eff61bb6344445
Alison Gleeson <agleeson987@gmail.com>
2025-08-22 20:10:28 +0100

    Problem 3

---
+++ 11 - Strings/03-CPPSubstring.cpp
@@ -0,0 +1,43 @@
+/*
+Given a string, s, and two integers, L and R, print the characters in the string from position L to position R
+*/
+
+#include <iostream>
+
+std::string substring(std::string &s, int L, int R);
+
+int main() {
+    std::string str;
+    int l, r;
+
+    //getting string from user
+    std::cout << "Enter a string:\n";
+    getline(std::cin, str);
+
+    //getting l and r from user
+    std::cout << "\nEnter two integers:\n";
+    std::cin >> l;
+    std::cin >> r;
+
+    //if left is greater than right, swap them
+    if (l > r) {
+        int temp = l;
+        l = r;
+        r = temp;
+    }
+
+    std::cout << "\n" << substring(str, l, r);
+
+
+    return 0;
+}
+
+std::string substring(std::string &s, int L, int R) {
+    std::string result = "";
+
+    for (int i=L; i<=R; i++) {
+        result += s[i];
+    }
+
+    return result;
+}
