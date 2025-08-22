f507192513e29fe1b9ca8f91d36749675af7c966
Alison Gleeson <agleeson987@gmail.com>
2025-08-22 19:09:42 +0100

    Problem 2

---
+++ 11 - Strings/02-FirstOccurence.cpp
@@ -0,0 +1,61 @@
+/*
+Given two strings, txt and pat, return the index of the first occurrence of substring pat in txt. If pat is not found, return -1
+*/
+
+#include <iostream>
+
+int substring(std::string& txt, std::string& pat);
+
+int main() {
+    std::string pat = "He Hell World Hello";
+    std::string txt = "Hello";
+    int pos;
+
+    //storing position found from substring() in pos
+    pos = substring(pat, txt);
+
+    //if pos is equal to -1, pat is not within txt
+    if (pos == -1) {
+        std::cout << "Not present";
+    } else {
+        std::cout << "Present at position " << pos;
+    }
+
+    return 0;
+}
+
+int substring(std::string& txt, std::string& pat) {
+    int index;
+    int sizeT = txt.length();
+    int sizeP = pat.length();
+
+
+    //looping through txt
+    for (int i=0; i<sizeT; i++) {
+
+        //if first character of pat is found in txt
+        if (pat[0] == txt[i]) {
+
+            //store position in index
+            index = i;
+
+            //check for each subsequent letter
+            for (int j = 1; j<sizeP; j++) {
+
+                //if a non-match is found, set index back to -1 and break from this loop
+                if (pat[j] != txt[i+j]) {
+                    index = -1;
+                    break;
+                }
+
+                //if the last letter of pat was checked and loop not broken, return index to prevent further searching
+                if (j == sizeP-1) {
+                    return index;
+                }
+            }
+        }
+    }
+
+    //returning position or -1
+    return index;
+}
