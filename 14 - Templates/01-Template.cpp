01ef3e5ecb9d76903429d8fdf762609f9a56fd03
Alison Gleeson <agleeson987@gmail.com>
2025-09-03 19:07:14 +0100

    Problem 1 - Adding main

--- 14 - Templates/01-Template.cpp
+++ 14 - Templates/01-Template.cpp
@@ -5,23 +5,43 @@ Create a class called minElement, which will include:
     - A method, check(), which would take a parameter, x, and print the smaller variable between x and y
 */
 
+#include <iostream>
+
 template <class T>
-class minElement {
-    //private variable, y
-    T y; 
+    class minElement {
+        //private variable, y
+        T y; 
 
-    public:
-        //default constructor
-        minElement(){};
+        public:
+            //default constructor
+            minElement(){};
 
-        //parameterized constructor
-        minElement(T a){
-            y = a;
-        }
+            //parameterized constructor
+            minElement(T a){
+                y = a;
+            }
 
-        //method to check and output the minimum of two elements
-        void check(T x)
-        {
-            std::cout << (y<x ? y : x) << std::endl;
-        }   
-};
+            //method to check and output the minimum of two elements
+            void check(T x)
+            {
+                std::cout << (y<x ? y : x) << std::endl;
+            }   
+    };
+
+
+int main() {
+
+    //creating an object of minElement, wherein y is an integer
+    minElement<int> first(5);
+
+    //calling the check() method in minElement, passing in 3
+    first.check(3);
+
+    //creating an object of minElement, wherein y is a string
+    minElement<std::string> second("abc");
+
+    //calling the check() method in minElement, passing in 3
+    second.check("abcd");
+
+    return 0;
+}
